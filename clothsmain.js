3c8e2eb5b2a22fd4db88c5afb552fa039f9db183
Rudra-00 <82320714+Rudra-00@users.noreply.github.com>
2022-04-03 17:47:19 +0530

    Add files via upload

---
+++ clothsmain.js
@@ -0,0 +1,12 @@
+function thesouledstore(){
+    window.location="https://www.thesouledstore.com/?utm_source=google&utm_medium=cpc&utm_campaign=TSS_Search_Brand_RM&utm_adgroup=Core_Brand&utm_term=the%20souled%20store&utm_network=g&utm_matchtype=e&utm_device=c&gclid=CjwKCAjwi6WSBhA-EiwA6Niok_GAKZ9Je2xJGc4OQALvyr6fK6nJ1DUgLytmvHn-p_odeRK_QueM2xoCkrIQAvD_BwE";
+}
+function kingsstudio(){
+    window.location="https://www.kingsstudio.in/";
+}
+function fugazee(){
+    window.location="https://www.fugazee.com/?utm_source=adyogi&utm_medium=google-search&utm_campaign=XYZF_3072_Adyogi_Brand-Search_Engage_-12419759985";
+}
+function urbanic(){
+    window.location="https://www.urbanic.com/?utm_source=google&utm_medium=cpc&utm_campaign=google_web_search_urbanic&utm_content=google_web_search_urbanic&utm_term=urbanic&gclid=CjwKCAjwi6WSBhA-EiwA6Niok37nEv8GhxuoSIOjtvNgYsZufbSizR7_nDFRyR5Lzy_zd8kOZ3ZcqBoCFWIQAvD_BwE";
+}
---
+++ main.js
@@ -0,0 +1,15 @@
+function amazon(){
+    window.location="https://www.amazon.in/?&ext_vrnc=hi&tag=googinhydr1-21&ref=pd_sl_4b3e4wrxds_b&adgrpid=60639568242&hvpone=&hvptwo=&hvadid=294102729729&hvpos=&hvnetw=g&hvrand=2321300555484809469&hvqmt=b&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9062215&hvtargid=kwd-311187680635&hydadcr=5841_1914865&gclid=CjwKCAjwi6WSBhA-EiwA6NiokxuQTxQRm6tPv1lXhZt8BpAIrji2vTQ2m5hC5sW_Wqt3VkUsasdBHxoCgpAQAvD_BwE";
+}
+function flipkart(){
+    window.location="https://www.flipkart.com/?s_kwcid=AL!739!3!582822043916!e!!g!!flipkart&gclsrc=aw.ds&&semcmpid=sem_8024046704_brand_exe_buyers_goog&gclid=CjwKCAjwi6WSBhA-EiwA6Niok4XK9Bj2bm1SOArw5oM4cBFEYXk4eT7Ip0ySvFz0nLBl1PQRBE1LhBoCBOcQAvD_BwE";
+}
+function myntra(){
+    window.location="https://www.myntra.com/?utm_source=perf_google_search_brand&utm_medium=perf_google_search_brand&utm_campaign=Search%20-%20Myntra%20Brand%20(India)&gclid=CjwKCAjwi6WSBhA-EiwA6Niok8-ymmEa157rxt1qEVmKpPmLiniYav8S9DmLDRZQOnGP3ad0iFoBsRoC3w0QAvD_BwE";
+}
+function meesho(){
+    window.location="https://meesho.com/";
+}
+function cloths(){
+    window.location="clothsindex.html";
+}
